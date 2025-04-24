fc75ea885b79a28abe81fd478a4f2ee48ac0d6fa
Santiago Brandes <santiagobrandes@gmail.com>
2025-04-24 08:28:25 -0300

    implementacion args

--- argumentos/arguments.py
+++ argumentos/arguments.py
@@ -1,1 +1,9 @@
-print("juancito")
+"""
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="input file", required=True)
+parser.add_argument("-o", "--output", help="output file", required=True)
+
+args = parser.parse_args()
