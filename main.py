a90b10338f26b417d2d5d289a4c86f67a71c7b2e
yizuz-maker <jesuscambronero2019@gmail.com>
2025-05-12 01:08:23 -0300

    [ADD] Implementacion prematura de Argparse

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+import argparse
 from scanner.network_scanner import escanear_puertos
 from matrices.operaciones_matriz import crear_matriz, mostrar_matriz
 from listas.operaciones_listas import obtener_puertos_abiertos, contar_puertos_abiertos, ordenar_por_puerto
-from cadenas.strings_utils import limpiar_ip, obtener_prefix 
+from cadenas.strings_utils import limpiar_ip, obtener_prefix
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="PortSpy - Escaner de puertos en Python")
+    parser.add_argument("--ip", required=True, help="Direccion IP a escanear")
+    parser.add_argument("--rango", required=True, help="Rango de puertos a escanear (ej: 20-80)")
+    return parser.parse_args()
 
 def main():
-    ip = limpiar_ip(input("Ingrese la IP a escanear: "))
-    rango = input("Ingresa el rango de puertos (ej: 20-25): ")
+    args = parse_args()
 
-    inicio,fin = map(int, rango.split('-')) # rango pasa a ser ["20", "25"] y luego se mapean para que se pasen a INT
-    puertos = list(range(inicio, fin + 1)) # Puertos es una lista tipo [20, 21, 22, 23, 24, 25]
+    ip = limpiar_ip(args.ip)
+    inicio, fin = map(int, args.rango.split('-')) # rango pasa a ser ["20", "25"] y luego se mapean para que se pasen a INT
+    puertos = list(range(inicio, fin + 1))  # Puertos es una lista tipo [20, 21, 22, 23, 24, 25]
 
-    resultados = escanear_puertos(ip, puertos) 
+    resultados = escanear_puertos(ip, puertos)
 
     matriz = crear_matriz(ip, resultados)
     mostrar_matriz(matriz)
 
     abiertos = obtener_puertos_abiertos(matriz)
-
     print(f"\nPuertos abiertos detectados: {abiertos}")
 
     total_abiertos = contar_puertos_abiertos(matriz)
@@ -29,8 +35,6 @@ def main():
             print(f"{fila[0]:<15} {fila[1]:<10} {fila[2]:<10} {fila[3]:<10}")
 
     print(f"\nPrefijo de la red: {obtener_prefix(ip)}")
-    
-if __name__ == "__main__": # Define que este archivo sea ejecutable
+
+if __name__ == "__main__":
     main()
-
-
