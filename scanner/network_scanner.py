9872a6fe2760835b5b501bf2908dac4340799c0a
Fedecastillom <Fmascm20@gmail.com>
2025-05-14 00:26:11 -0300

    [ADD] Uso de hilos en el escaneo de puertos

--- scanner/network_scanner.py
+++ scanner/network_scanner.py
@@ -1,4 +1,5 @@
-import socket 
+import socket
+from concurrent.futures import ThreadPoolExecutor
 from scanner.banner_grabbing import obtener_banner, decodificar_banner, obtener_banner_http, determinar_http_service
 """
 Escanea los puertos de una IP para determinar su estado y obtener el banner del servicio si está abierto.
@@ -9,49 +10,53 @@ Escanea los puertos de una IP para determinar su estado y obtener el banner del 
         - estado es 'abierto' o 'cerrado'
         - banner contiene una cadena con información del servicio (vacía si está cerrado)
 """
-def escanear_puertos(ip, puertos):
-    resultados = []
+def escanear_puerto(ip, puerto):
     banner_decoded = ""
 
-    for puerto in puertos:
-        banner_decoded = ""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(5)
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(5)
+    try:
+        resultado = sock.connect_ex((ip, puerto))  
 
-        try:
-            resultado = sock.connect_ex((ip, puerto))  
+        if resultado == 0:
+            estado = 'abierto'
 
-            if resultado == 0:
-                estado = 'abierto'
-
-                if puerto == 80:
-                    sock = obtener_banner_http(ip, sock)
-                    banner_raw = obtener_banner(sock)
-                    banner_dirty = decodificar_banner(banner_raw)
-                    banner_decoded = determinar_http_service(banner_dirty)
-                else:
-                    banner_raw = obtener_banner(sock)
-                    banner_decoded = decodificar_banner(banner_raw)
-
-                resultados.append((puerto, estado, banner_decoded))
+            if puerto == 80:
+                sock = obtener_banner_http(ip, sock)
+                banner_raw = obtener_banner(sock)
+                banner_dirty = decodificar_banner(banner_raw)
+                banner_decoded = determinar_http_service(banner_dirty)
             else:
-                estado = 'cerrado'
-                resultados.append((puerto, estado, banner_decoded))
+                banner_raw = obtener_banner(sock)
+                banner_decoded = decodificar_banner(banner_raw)
 
-        except socket.timeout:
-
+            return (puerto, estado, banner_decoded)
+        else:
             estado = 'cerrado'
-            resultados.append((puerto, estado, banner_decoded))
-            print(f"Timeout al intentar conectar al puerto {puerto} en {ip}")
+            return (puerto, estado, banner_decoded)
 
-        except socket.error as e:
+    except socket.timeout:
+        estado = 'cerrado'
+        print(f"Timeout al intentar conectar al puerto {puerto} en {ip}")
+        return (puerto, estado, banner_decoded)
 
-            estado = 'cerrado'
-            resultados.append((puerto, estado, banner_decoded))
-            print(f"Error de socket al intentar conectar al puerto {puerto} en {ip}: {e}")
+    except socket.error as e:
+        estado = 'cerrado'
+        print(f"Error de socket al intentar conectar al puerto {puerto} en {ip}: {e}")
+        return (puerto, estado, banner_decoded)
 
-        finally:
-            sock.close()  # Cerrar el socket
+    finally:
+        sock.close()
+
+def escanear_puertos(ip, puertos):
+    resultados = []
+
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for puerto in puertos:
+            futuros = [executor.submit(escanear_puerto, ip, puerto)]
+
+        for futuro in futuros:
+            resultados.append(futuro.result())
 
     return resultados
